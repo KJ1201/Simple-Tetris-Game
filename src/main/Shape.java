38e0ac1c26a45cfe5f12a6db1ec8ea991d692b4a
KJ1201 <karanjuneja2006@outlook.com>
2026-04-20 22:37:19 +0530

    Add coordinate table and setShape to Shape

--- src/main/Shape.java
+++ src/main/Shape.java
@@ -2,16 +2,36 @@ package main;
 
 public class Shape {
 
-    protected enum Tetromino {
+    protected enum Tetrominoe {
         NoShape, ZShape, SShape, LineShape,
         TShape, SquareShape, LShape, MirroredLShape
     }
 
-    private Tetromino pieceShape;
+    private Tetrominoe pieceShape;
+    private int[][] coords;
+    private int[][][] coordsTable;
 
     public Shape() {
-        pieceShape = Tetromino.NoShape;
+        coords = new int[4][2];
+        coordsTable = new int[][][] {
+                { {0,0},   {0,0},   {0,0},  {0,0}  },
+                { {0,-1},  {0,0},  {-1,0}, {-1,1}  },
+                { {0,-1},  {0,0},   {1,0},  {1,1}  },
+                { {0,-1},  {0,0},   {0,1},  {0,2}  },
+                { {-1,0},  {0,0},   {1,0},  {0,1}  },
+                { {0,0},   {1,0},   {0,1},  {1,1}  },
+                { {-1,-1}, {0,-1},  {0,0},  {0,1}  },
+                { {1,-1},  {0,-1},  {0,0},  {0,1}  }
+        };
+        setShape(Tetrominoe.NoShape);
     }
 
-    public Tetromino getShape() { return pieceShape; }
+    protected void setShape(Tetrominoe shape) {
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 2; j++)
+                coords[i][j] = coordsTable[shape.ordinal()][i][j];
+        pieceShape = shape;
+    }
+
+    public Tetrominoe getShape() { return pieceShape; }
 }

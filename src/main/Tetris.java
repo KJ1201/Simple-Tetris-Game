83376724e662baecfffde3c7cacff29c14279dbb
KJ1201 <karanjuneja2006@outlook.com>
2026-04-20 22:34:53 +0530

    Add status bar label to bottom of window

--- src/main/Tetris.java
+++ src/main/Tetris.java
@@ -1,21 +1,30 @@
 package main;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Tetris extends JFrame {
+
+    private JLabel statusbar;
 
     public Tetris() {
         initUI();
     }
 
     private void initUI() {
-        setTitle("Tetris");
-        setSize(200, 400);
+        statusbar = new JLabel(" 0");
+        add(statusbar, BorderLayout.SOUTH);
+
+        setTitle("Java Project - 24BCS10791, 24BCS10403, 24BCS107XX, 24BCS107XX");
+        setSize(600, 800);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
     }
+
+    JLabel getStatusBar() { return statusbar; }
 
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> new Tetris());
